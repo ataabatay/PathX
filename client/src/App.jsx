631aa09f2e844a3f796ddc02bad5458fcd4a9f23
Ata Abatay <aliataabatay@gmail.com>
2024-01-15 20:44:48 +0000

    initial version of the navbar done, to do: responsiveness + links navigating to pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,11 @@
-import { useState } from 'react'
+// Component imports
+import Navbar from './components/Navbar.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
     </>
   )
 }
-
-export default App
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,24 @@
+import '../styles/components/navbar/Navbar.css'
 
 export default function Navbar() {
+
   return (
     <>
+      <section className="navbar">
+        <div className="navbar-logo">
+          <h1>PathX</h1>
+        </div>
+        <div className="navbar-links">
+          <ul>
+            <li>My Sessions</li>
+            <li>Book</li>
+            <li>About us</li>
+            <li>FAQ</li>
+            <li>Contact us</li>
+            <li>Login</li>
+          </ul>
+        </div>
+      </section>
     </>
   )
 }
