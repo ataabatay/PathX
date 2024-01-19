04c0d5791b3ef422b63665ebd688a1246c89c9ac
Ata Abatay <aliataabatay@gmail.com>
2024-01-19 12:42:33 +0000

    My sessions page is done with designs and functionality and now shows all the sessions so a tweak will need to be done so it only shows your sessions

--- client/src/components/MySessions.jsx
+++ client/src/components/MySessions.jsx
@@ -1,7 +1,15 @@
 import { useEffect } from "react"
 import { useLoaderData } from "react-router-dom"
+import '../styles/components/mysessions/MySessions.css'
+import EditButton from '../assets/icons/editicon.svg';
+import DeleteButton from '../assets/icons/deleteicon.svg';
 
-
+const options = {
+  weekday: "long",
+  year: "long",
+  month: "long",
+  day: "long",
+};
 
 export default function MySessions() {
 
@@ -11,24 +19,35 @@ export default function MySessions() {
   const sessions = res.sessions
 
   useEffect(() => {
-    console.log(res)
-    console.log(coaches, session_types, sessions)
+    // console.log(res)
+    console.log(coaches[0].name)
   }, [])
 
   return (
     <>
       <section className="mysessions">
-        <h1>My sesssshhhhss</h1>
+        <h1>My Sessions</h1>
         <section className="activesessions">
           {sessions.map(session => {
+            const coach = coaches.find(coach => coach.id === session.coach);
+            const sessionType = session_types.find(type => type.id === session.session_type);
             return (
               <div className="uniquesession" key={session.id}>
-                <p className="date">{session.scheduled_date}</p>
-                <p className="time">{session.scheduled_time}</p>
-                {/* <p className="coach">{coaches[session.coach].name}</p> */}
-                <p className="sessiontype">{session.session_type}</p>
+                <div className="session-details">
+                  <p className="coach-sessiontype"><strong>{sessionType ? sessionType.name : 'Unknown Session Type'}</strong> with {coach ? coach.name : 'Unknown Coach'}</p>
+                  <p className="date-time">
+                    {new Date(session.scheduled_date).toDateString()}, {session.scheduled_time}</p>
+                </div>
+                <div className="icons">
+                  <div className="editbutton">
+                    <img src={EditButton} alt="edit-button" />
+                  </div>
+                  <div className="deletebutton">
+                    <img src={DeleteButton} alt="delete-button" />
+                  </div>
+                </div>
               </div>
-            )
+            );
           })}
         </section>
         <section className="pastsessions">
